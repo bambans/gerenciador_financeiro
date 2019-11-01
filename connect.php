d3ae069dbfaf3107f0cf5bae587e19fc5a59a151
otaviobambans <43711613+otaviobambans@users.noreply.github.com>
2019-11-01 10:51:40 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 	$servidor = 'localhost';
-	$usuario = 'id11205855_root';
-	$senha = 'bro21072002';
-	$banco = 'id11205855_db_financas';
+	$usuario = 'root';
+	$senha = '';
+	$banco = 'db_financas';
 	$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
 	$mysqli->set_charset('utf8');
 	if(mysqli_connect_errno()) trigger_error(mysqli_connect_error());
